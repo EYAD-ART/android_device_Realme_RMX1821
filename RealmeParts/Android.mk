0c91bfabe1f1e2d4c86886b2fecbf13ffe052d29
EYAD <33518165+EYAD-ART@users.noreply.github.com>
2020-03-30 22:37:56 +0200

    uodate doze test fix fp

--- RealmeParts/Android.mk
+++ RealmeParts/Android.mk
@@ -12,7 +12,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := doze
+LOCAL_PACKAGE_NAME := RealmeParts
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
@@ -20,12 +20,8 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v4 \
-    android-support-v13 \
-    android-support-v7-recyclerview \
-    android-support-v7-preference \
-    android-support-v7-appcompat \
-    android-support-v14-preference
+    androidx.core_core \
+    androidx.preference_preference
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res
