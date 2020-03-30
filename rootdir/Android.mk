bb9ed71a2be25bbfd4bc841f3b62154e1ae15fc6
EYAD <33518165+EYAD-ART@users.noreply.github.com>
2020-03-30 23:15:49 +0200

    fingerprint :add some stuff

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,3 +17,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.mt6771
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oppo.vendor.fingerprint.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.oppo.vendor.fingerprint.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
